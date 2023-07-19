b68edc870cbdfb0758cb82dfa24f680d89dfed4c
malaikaikartel <malaikaikartel123@gmail.com>
2023-07-19 02:06:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/consts/app-config.ts
@@ -0,0 +1,29 @@
+const pages = [
+  {
+    path: "/",
+    title: "Home",
+    metadata: {
+      title: "Anıl Şenay",
+      description: "Anıl Şenay Personal Website",
+    },
+  },
+  {
+    path: "/projects",
+    title: "Projects",
+    metadata: {
+      title: "Projects | Anıl Şenay",
+      description: "Some of my personal projects in Github",
+    },
+  },
+  {
+    path: "/about",
+    title: "About",
+    metadata: {
+      title: "About | Anıl Şenay",
+      description:
+        "Software Engineer, JavaScript Enthusiast, Software Craftsman, React Lover, Gopher, Gamer, Geek",
+    },
+  },
+];
+
+export { pages };
---
+++ src/pages/about/components/AboutMe/about-me.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./about-me.module.scss";
+
+type Props = {};
+
+export default function AboutMe({}: Props) {
+  return (
+    <div className={styles.container}>
+      <h4>About Me</h4>
+      <div className={styles.text}>
+        <p>
+          Hi, I am Anıl. I am a software engineer at Cimri.
+          <br />
+          <br />
+          I graduated from Marmara University Computer Science & Engineering at
+          2022. I've been interested in computers and programming since my
+          childhood. Coding and design, which I started with curiosity and
+          passion, is now a big part of my life.
+          <br />
+          <br />
+          I can describe myself as a Geek. Computer games, movies, comics,
+          science fiction and fantasy books are my interests that I spare time
+          in my daily life.
+          <br />
+          <br />
+          I took the first step into the software world with web designing at my
+          early ages. After dealing with web design for a few years, I had not
+          do any development on the web for a long time. In recent years, I am
+          working as developer professionally.
+          <br />
+          <br />I am mainly interested in front-end development by using React,
+          React Native and back-end development by using Golang.
+        </p>
+      </div>
+    </div>
+  );
+}
